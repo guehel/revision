c1cba4642411e38e0fe87492e6a822d39da013c4
boug18087415 <boug18087415@LQ2108-04.cegeplimoilou.lan>
2014-08-05 08:47:29 -0400

    last version   de vote

--- cookiesVotes/vote.php
+++ cookiesVotes/vote.php
@@ -6,7 +6,8 @@
  */
 
 class Sondage {
-    private $fichierSondage ='sondage.txt';
+
+    private $fichierSondage = 'sondage.txt';
 
     public function verifierVote() {
         $aVote = isset($_COOKIE['vote']);
@@ -14,64 +15,74 @@ class Sondage {
     }
 
     public function enegistrerVote() {
-        if(isset($_GET['vote'])){
-        $vote = $_GET['vote'];
-        $this->enregistrerVotant($vote);
-        setcookie('vote', $vote, time()+ 36000 * 2);
+        if (isset($_GET['vote'])) {
+            $vote = $_GET['vote'];
+            $this->enregistrerVotant($vote);
+            setcookie('vote', $vote, time() + 36000 * 2);
         }
     }
-    
+
     /** lier le cookie vote
-    */
-    public function obtenirVote(){
-       
-            return $_COOKIE['vote'];
-       
+     */
+    public function obtenirVote() {
+
+        return $_COOKIE['vote'];
     }
+
     /** ecrie le texte vote ds le fichier sondage
-    */
+     */
     private function enregistrerVotant($vote) {
-        $sondage = fopen($fichierSondage, 'a');
-        fwrite($sondage, $vote . '\n');
+        $sondage = fopen($this->fichierSondage, 'a');
+        if (!empty($vote))
+            fwrite($sondage, $vote . '\n');
         fclose($sondage);
     }
-    /**retourne un tableu indexe du nombre de votes par choix
-    */
-    public function lireVotes(){
+
+    /*     * retourne un tableu indexe du nombre de votes par choix
+     */
+
+    public function lireVotes() {
         $php = 0;
-        $asp= 0;
+        $asp = 0;
         $jsp = 0;
-        $sondage = compact('php', 'asp', 'asp');
+        $sondage = compact('php', 'jsp', 'asp');
         $listeDesVotes = $this->listerVotes();
-        $sondage  = $this->compterVotes($sondage, $listeDesVotes);
-        return $sondage;
-    }
-    /*lie le fichier et renvoie un tableau du contenu
-    */
-     private function listerVotes() {
-        $sondage = fopen($fichierSondage, 'r');
-        $fileContent = fread($sondage, filesize($fichierSondage));
-        fclose($sondage);
-        return explode('\n', $fileContent );
-    }
-    /**incremente le tableau des sondages selon les votes */
-    private function compterVotes($sondage, $listeDesVote){
-        foreach ($listeDesVote as $vote){
-            $sondage[$vote]++;
-        }
+        $this->compterVotes($sondage, $listeDesVotes);
         return $sondage;
     }
 
+    /* lie le fichier et renvoie un tableau du contenu
+     */
+
+    private function listerVotes() {
+        $fileContent = '';
+        if (file_exists($this->fichierSondage)) {
+            $sondage = fopen($this->fichierSondage, 'r');
+            $fileContent = fread($sondage, filesize($this->fichierSondage));
+            fclose($sondage);
+        }
+        return explode('\n', $fileContent);
+    }
+
+    /*     * incremente le tableau des sondages selon les votes */
+
+    private function compterVotes(&$sondage, $listeDesVote) {
+        foreach ($listeDesVote as $vote) {
+            if (!empty($vote))
+                $sondage[$vote] = $sondage[$vote]+1 ;
+        }
+    }
+
 }
-    /**Controleur de l'application */
+
+/* * Controleur de l'application */
 $sondage = new Sondage();
 $aVote = $sondage->verifierVote();
-$vote ;
+$vote;
 if (!$aVote) {
     $sondage->enegistrerVote();
-}else{
-  $vote  = $sondage->obtenirVote();
-  
+} else {
+    $vote = $sondage->obtenirVote();
 }
-$reponses = $sondage->listerVotes() ;
+$reponses = $sondage->lireVotes();
 include 'vueVote.php';
